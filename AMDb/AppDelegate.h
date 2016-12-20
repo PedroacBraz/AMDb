bfcd4e2ab18d521f3c806b0b88ef2e3fea447dcc
Mac Mini Beta <macminibeta@Macs-Mac-mini.local>
2016-12-20 14:45:59 -0200

    Initial Commit

---
+++ AMDb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMDb
+//
+//  Created by Mac Mini Beta on 20/12/16.
+//  Copyright © 2016 Mac Mini Beta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AMDb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMDb
+//
+//  Created by Mac Mini Beta on 20/12/16.
+//  Copyright © 2016 Mac Mini Beta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
