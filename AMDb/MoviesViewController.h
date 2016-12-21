8211806a5a85fb788871ab59643acb625cc6f042
Mac Mini Beta <macminibeta@Macs-Mac-mini.local>
2016-12-21 11:27:27 -0200

    Setting up screens

---
+++ AMDb/MoviesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesViewController.h
+//  AMDb
+//
+//  Created by Mac Mini Beta on 21/12/16.
+//  Copyright © 2016 Mac Mini Beta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesViewController : UITableViewController
+
+@end
